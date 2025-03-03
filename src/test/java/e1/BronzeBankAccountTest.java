e661e2b9c46d095f1a2520a83bba91ac8ded59fb
alessandro.agosta <alessandro.agosta@studio.unibo.it>
2025-03-03 11:17:57 +0100

    refactor

--- src/test/java/e1/BronzeBankAccountTest.java
+++ src/test/java/e1/BronzeBankAccountTest.java
@@ -6,18 +6,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BronzeBankAccountTest extends BaseBankAccountTest{
+
+    public static final int WITHDRAWAL_FEE_THRESHOLD = 100;
+
     @BeforeEach
     void init(){
         this.account = new BronzeBankAccount(new CoreBankAccount());
     }
 
     @Test
-    public void DontApplyFeeIfUnderThreshold(){
+    public void dontApplyFeeIfUnderThreshold(){
         this.account.deposit(INITIAL_DEPOSIT_AMOUNT);
 
-        this.account.withdraw(100);
+        this.account.withdraw(WITHDRAWAL_FEE_THRESHOLD);
 
-        final int expectedBalance = INITIAL_BALANCE_AMOUNT + INITIAL_DEPOSIT_AMOUNT - 100;
+        final int expectedBalance = INITIAL_BALANCE_AMOUNT + INITIAL_DEPOSIT_AMOUNT - WITHDRAWAL_FEE_THRESHOLD;
         assertEquals(expectedBalance, this.account.getBalance());
     }
 }
