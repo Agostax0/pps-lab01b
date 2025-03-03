8ff709c9a75db23f9fa2d08e665f2de41285e0ec
alessandro.agosta <alessandro.agosta@studio.unibo.it>
2025-03-03 11:17:16 +0100

    dont apply fee if under threshold

--- src/test/java/e1/BronzeBankAccountTest.java
+++ src/test/java/e1/BronzeBankAccountTest.java
@@ -1,10 +1,23 @@
 package e1;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BronzeBankAccountTest extends BaseBankAccountTest{
     @BeforeEach
     void init(){
         this.account = new BronzeBankAccount(new CoreBankAccount());
+    }
+
+    @Test
+    public void DontApplyFeeIfUnderThreshold(){
+        this.account.deposit(INITIAL_DEPOSIT_AMOUNT);
+
+        this.account.withdraw(100);
+
+        final int expectedBalance = INITIAL_BALANCE_AMOUNT + INITIAL_DEPOSIT_AMOUNT - 100;
+        assertEquals(expectedBalance, this.account.getBalance());
     }
 }
